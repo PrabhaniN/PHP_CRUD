92d5598385597a8241fa8f6947c948585c80700c
Prabhani Nanayakkara <prabhaninanayakkara1907@gmail.com>
2019-03-20 10:21:49 +0530

    Created database connection using php and changed the file location

---
+++ add_stock.php
@@ -0,0 +1,20 @@
+<?php
+include_once 'conn.php';
+
+$mat_name = $_POST['mat-name'];
+$mat_code = $_POST['mat-code'];
+$no_of_colors = $_POST['no-of-colours'];
+$quantity = $_POST['quantity'];
+$price = $_POST['price'];
+
+$sql = "INSERT INTO materials(mat_code, mat_name, no_of_colors, quantity, price) VALUES ('$mat_code', '$mat_name', '$no_of_colors', '$quantity', '$price')";
+
+if ($conn->query($sql) === TRUE){
+    echo "New record added";
+    header('Location:index.php');
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
